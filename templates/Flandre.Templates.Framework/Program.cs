5433a418a6504aa14ad0eb9adfb4d20602b32cc6
b1acksoil <dev@sorabs.cc>
2023-02-25 03:55:14 +0800

    feat: initial commit

---
+++ templates/Flandre.Templates.Framework/Program.cs
@@ -0,0 +1,21 @@
+﻿using Flandre.Framework;
+using Flandre.Templates.Framework;
+using Microsoft.Extensions.Hosting;
+
+var builder = FlandreApp.CreateBuilder(args);
+
+// 安装一个适配器，并添加在这里。
+// builder.AddAdapter(new YourAdapter());
+
+builder.AddPlugin<ExamplePlugin>();
+
+var app = builder.Build();
+
+// 添加内置中间件。
+// 这些中间件保证 Flandre 的正常运转。你也可以加入自己的中间件，并灵活调整其顺序。
+app.UseCommandSession();
+// app.UseMiddleware((ctx, next) => { /* ... */ });
+app.UseCommandParser();
+app.UseCommandInvoker();
+
+app.Run();
