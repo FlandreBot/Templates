29fa87f8e734bf663056013c0f6ee132c5f47107
b1acksoil <dev@sorabs.cc>
2023-02-28 22:01:22 +0800

    feat(template-fx): add appsettings.json

--- templates/Flandre.Templates.Framework/Program.cs
+++ templates/Flandre.Templates.Framework/Program.cs
@@ -14,7 +14,7 @@ var app = builder.Build();
 // 添加内置中间件。
 // 这些中间件保证 Flandre 的正常运转。你也可以加入自己的中间件，并灵活调整其顺序。
 app.UseCommandSession();
-// app.UseMiddleware((ctx, next) => { /* ... */ });
+// app.UseMiddleware(async (ctx, next) => { /* ... */ });
 app.UseCommandParser();
 app.UseCommandInvoker();
 
